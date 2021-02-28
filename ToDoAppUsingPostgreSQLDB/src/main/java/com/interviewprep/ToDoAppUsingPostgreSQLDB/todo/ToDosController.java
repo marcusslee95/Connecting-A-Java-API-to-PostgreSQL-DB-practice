50cc82825a0ef6861dcec39ab109fe059dd11a52
Marcus Lee <marcusslee95@berkeley.edu>
2021-02-28 10:10:59 -0800

    tested using a path parameter

--- ToDoAppUsingPostgreSQLDB/src/main/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/todo/ToDosController.java
+++ ToDoAppUsingPostgreSQLDB/src/main/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/todo/ToDosController.java
@@ -1,6 +1,7 @@
 package com.interviewprep.ToDoAppUsingPostgreSQLDB.todo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class ToDosController {
 	@GetMapping("/")
 	public String sampleEndpoint() {
 		return "Testing if this web service is receiving requests properly";
+	}
+	
+	@GetMapping("/users/{id}/todos")
+	public String findAllToDosOfAUser(@PathVariable int id) {
+		return "User id is " + id;
 	}
 	
 
