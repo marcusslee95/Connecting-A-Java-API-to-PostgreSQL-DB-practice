d017889e4a108834ba1239d5100d80d2edf91514
Marcus Lee <marcusslee95@berkeley.edu>
2021-02-27 23:29:56 -0800

    created spring boot project using spring initializer. Included all the dependencies I need to create a web service that will interact w/a postgreSQL db

---
+++ ToDoAppUsingPostgreSQLDB/src/main/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/ToDoAppUsingPostgreSqldbApplication.java
@@ -0,0 +1,13 @@
+package com.interviewprep.ToDoAppUsingPostgreSQLDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppUsingPostgreSqldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppUsingPostgreSqldbApplication.class, args);
+	}
+
+}
---
+++ ToDoAppUsingPostgreSQLDB/src/test/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/ToDoAppUsingPostgreSqldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewprep.ToDoAppUsingPostgreSQLDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppUsingPostgreSqldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
