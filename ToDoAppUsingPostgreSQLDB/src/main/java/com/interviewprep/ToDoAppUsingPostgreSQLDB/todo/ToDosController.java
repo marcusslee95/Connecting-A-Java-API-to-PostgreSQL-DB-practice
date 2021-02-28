a00fdc0cbb3360cdafbb876a9fba478892801d66
Marcus Lee <marcusslee95@berkeley.edu>
2021-02-28 15:44:57 -0800

    implemented find a particular todo of a particular user endpoint

--- ToDoAppUsingPostgreSQLDB/src/main/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/todo/ToDosController.java
+++ ToDoAppUsingPostgreSQLDB/src/main/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/todo/ToDosController.java
@@ -1,6 +1,7 @@
 package com.interviewprep.ToDoAppUsingPostgreSQLDB.todo;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +29,25 @@ public class ToDosController {
 //		return "Username is: " + username;
 //		return thingThatInteractsWithDB.findAll();
 		return thingThatInteractsWithDB.findByUsername(username);
+	}
+	
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todos findAToDoOfAUser(@PathVariable String username, @PathVariable int id) {
+		List<Todos> allTodosOfThisUser = thingThatInteractsWithDB.findByUsername(username);
+		
+		for (Todos todo : allTodosOfThisUser) 
+		{ 
+		    if (todo.getId() == id) {
+		    	return todo;
+		    }
+		}
+		
+		throw new NoSuchElementException("User doesn't have a todo of matching id or that user doesn't exist");
+		
+//		//could have used the findById method that we have but.... if did that then we wouldn't be checking for username
+//		//i.e. only http://localhost:8080/users/M/todos/1 should give me a todo for user M of ID 1 but... 
+//		//http://localhost:8080/users/dfasfdsfasdf/todos/1 would give the same todo as previous request url
+//		return thingThatInteractsWithDB.findById(id).get();
 	}
 	
 
