b0e3a7a2a86e9cbe8600990c279944434ae72438
Marcus Lee <marcusslee95@berkeley.edu>
2021-02-28 23:03:22 -0800

    added some instructions on how to work with Todo app if ever want to revisit

--- ToDoAppUsingPostgreSQLDB/src/main/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/todo/ToDosController.java
+++ ToDoAppUsingPostgreSQLDB/src/main/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/todo/ToDosController.java
@@ -18,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.example.ch5HelloWorld.todo.Todo;
-
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
 public class ToDosController {
