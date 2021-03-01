624263261f6afc6d3de9935dd9bc7e4d52a8bd5d
Marcus Lee <marcusslee95@berkeley.edu>
2021-02-28 22:49:57 -0800

    added the frontend react app to see if the Todo app - now connected to java backend that talks to a postgreSQL db not a h2 db - works. IT DOES

--- ToDoAppUsingPostgreSQLDB/src/main/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/todo/ToDosController.java
+++ ToDoAppUsingPostgreSQLDB/src/main/java/com/interviewprep/ToDoAppUsingPostgreSQLDB/todo/ToDosController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.example.ch5HelloWorld.todo.Todo;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class ToDosController {
 	
 	@Autowired 
---
+++ growth-team-interview-prep-app/src/requestSenders/ToDosRequestSender.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+
+class ToDosRequestSender {
+
+    //"/users/{username}/todos"
+    toGetAllTodosOfASpecificUserEndpoint(username){
+        return axios.get(`http://localhost:8080/users/${username}/todos`)
+        // return axios.get(`http://localhost:8080/jpa/users/${username}/todos`)
+    }
+
+    //"/users/{username}/todos"
+    toGetATodoEndpoint(username, id){
+        return axios.get(`http://localhost:8080/users/${username}/todos/${id}`)
+        // return axios.get(`http://localhost:8080/jpa/users/${username}/todos/${id}`)
+    }
+
+
+    //"/users/{username}/todos/{id}"
+    toDeleteATodoEndpoint(username, id){
+        return axios.delete(`http://localhost:8080//users/${username}/todos/${id}`)
+    }
+
+    //"/users/{username}/todos/{id}"
+    toUpdateATodoEndpoint(username, id, todo){
+        return axios.put(`http://localhost:8080//users/${username}/todos/${id}`, todo)
+    }
+
+    //"/users/{username}/todos"
+    toCreateATodoEndpoint(username, todo){
+        return axios.post(`http://localhost:8080//users/${username}/todos`, todo)
+    }
+
+}
+
+export default new ToDosRequestSender()
